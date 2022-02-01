eb0211cb17353849f24c10140620b458fd94c0d2
Sajid Hanif <shanif@aup.edu>
2022-02-01 20:47:27 +0100

    inc header missing

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,13 @@
+{{--<div class="jumbotron text-center jumbotron-image shadow" style="background-image: url('/hero-background.jpg');">--}}
+<header class="header">
+        <div class = "container">
+        <div class = "text-box">
+            <h1 class="heading-primary">
+                <span class="heading-primary-sub">WE'RE Infiniti Tech. We do</span>
+                <span class="heading-primary-main"> Network Support</span><br>
+                <span class="heading-primary-sub2"> We provide 24x7 onsite IT support </span>
+                <span class="heading-primary-sub2"> services in France, UK and Germany </span>
+            </h1>
+        </div>
+        </div>
+</header>
